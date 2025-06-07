533f6e299b472b7ad78bf1bcb3b758a367a58a25
Магомедали Гаджиев <mag198421@gmail.com>
2025-06-07 15:47:33 +0300

    Небольшие изменения

--- 2048WinFormsAppNew/InputForm.Designer.cs
+++ 2048WinFormsAppNew/InputForm.Designer.cs
@@ -20,57 +20,57 @@
 
         private void InitializeComponent()
         {
-            this.promptLabel = new System.Windows.Forms.Label();
-            this.inputTextBox = new System.Windows.Forms.TextBox();
-            this.okButton = new System.Windows.Forms.Button();
-            this.cancelButton = new System.Windows.Forms.Button();
+            this.promptLabel = new Label();
+            this.inputTextBox = new TextBox();
+            this.okButton = new Button();
+            this.cancelButton = new Button();
             this.SuspendLayout();
 
             // promptLabel
             this.promptLabel.AutoSize = true;
-            this.promptLabel.Location = new System.Drawing.Point(12, 20);
+            this.promptLabel.Location = new Point(12, 20);
             this.promptLabel.Name = "promptLabel";
-            this.promptLabel.Size = new System.Drawing.Size(46, 15);
+            this.promptLabel.Size = new Size(46, 15);
             this.promptLabel.TabIndex = 0;
             this.promptLabel.Text = "Промпт";
 
             // inputTextBox
-            this.inputTextBox.Location = new System.Drawing.Point(12, 48);
+            this.inputTextBox.Location = new Point(12, 48);
             this.inputTextBox.Name = "inputTextBox";
-            this.inputTextBox.Size = new System.Drawing.Size(260, 23);
+            this.inputTextBox.Size = new Size(260, 23);
             this.inputTextBox.TabIndex = 1;
 
             // okButton
-            this.okButton.Location = new System.Drawing.Point(116, 87);
+            this.okButton.Location = new Point(116, 87);
             this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.Size = new Size(75, 23);
             this.okButton.TabIndex = 2;
             this.okButton.Text = "OK";
             this.okButton.UseVisualStyleBackColor = true;
-            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            this.okButton.Click += new EventHandler(this.okButton_Click);
 
             // cancelButton
-            this.cancelButton.Location = new System.Drawing.Point(197, 87);
+            this.cancelButton.Location = new Point(197, 87);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.Size = new Size(75, 23);
             this.cancelButton.TabIndex = 3;
             this.cancelButton.Text = "Отмена";
             this.cancelButton.UseVisualStyleBackColor = true;
-            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            this.cancelButton.Click += new EventHandler(this.cancelButton_Click);
 
             // InputForm
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 126);
+            this.AutoScaleDimensions = new SizeF(7F, 15F);
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.ClientSize = new Size(284, 126);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.okButton);
             this.Controls.Add(this.inputTextBox);
             this.Controls.Add(this.promptLabel);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "InputForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.StartPosition = FormStartPosition.CenterParent;
             this.Text = "Игрок";
             this.ResumeLayout(false);
             this.PerformLayout();
--- 2048WinFormsAppNew/SizeSelectionForm.Designer.cs
+++ 2048WinFormsAppNew/SizeSelectionForm.Designer.cs
@@ -8,48 +8,48 @@
 
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.sizeComboBox = new System.Windows.Forms.ComboBox();
-            this.okButton = new System.Windows.Forms.Button();
+            this.label1 = new Label();
+            this.sizeComboBox = new ComboBox();
+            this.okButton = new Button();
             this.SuspendLayout();
 
             // label1
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Location = new Point(12, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 15);
+            this.label1.Size = new Size(136, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Выберите размер поля:";
 
             // sizeComboBox
-            this.sizeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.sizeComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
             this.sizeComboBox.FormattingEnabled = true;
-            this.sizeComboBox.Location = new System.Drawing.Point(12, 48);
+            this.sizeComboBox.Location = new Point(12, 48);
             this.sizeComboBox.Name = "sizeComboBox";
-            this.sizeComboBox.Size = new System.Drawing.Size(260, 23);
+            this.sizeComboBox.Size = new Size(260, 23);
             this.sizeComboBox.TabIndex = 1;
 
             // okButton
-            this.okButton.Location = new System.Drawing.Point(197, 87);
+            this.okButton.Location = new Point(197, 87);
             this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.Size = new Size(75, 23);
             this.okButton.TabIndex = 2;
             this.okButton.Text = "OK";
             this.okButton.UseVisualStyleBackColor = true;
-            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            this.okButton.Click += new EventHandler(this.okButton_Click);
 
             // SizeSelectionForm
-            this.ClientSize = new System.Drawing.Size(284, 126);
+            this.ClientSize = new Size(284, 126);
             this.Controls.Add(this.okButton);
             this.Controls.Add(this.sizeComboBox);
             this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.FormBorderStyle =  FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "SizeSelectionForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.StartPosition = FormStartPosition.CenterParent;
             this.Text = "Размер поля";
-            this.Load += new System.EventHandler(this.SizeSelectionForm_Load);
+            this.Load += new EventHandler(this.SizeSelectionForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
         }
