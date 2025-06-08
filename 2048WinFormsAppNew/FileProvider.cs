a998bf1790ebbd4b2085e26756965a957544983f
Магомедали Гаджиев <mag198421@gmail.com>
2025-06-08 18:26:39 +0300

    Добавил классы FileProvider и UserManager

---
+++ 2048WinFormsAppNew/FileProvider.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace _2048WinFormsAppNew
+{
+    public class FileProvider
+    {
+
+        public static void Replace(string fileName, string value)
+        {
+            var writer = new StreamWriter(fileName, false, Encoding.UTF8);
+            writer.WriteLine(value);
+            writer.Close();
+        }
+
+        public static string GetAll(string fileName)
+        {
+            var reader = new StreamReader(fileName, Encoding.UTF8);
+            var value = reader.ReadToEnd();
+            reader.Close();
+            return value;
+        }
+
+        public static bool Exists(string path)
+        {
+            return File.Exists(path);
+        }
+    }
+}
---
+++ 2048WinFormsAppNew/UserManager.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace _2048WinFormsAppNew
+{
+    
+    public class UserManager
+    {
+        public string Path = "resaults.json";
+        public List<User> GetALl()
+        {
+            var jsonData = FileProvider.GetAll(Path);
+            return JsonConvert.DeserializeObject<List<User>>(jsonData);
+        }
+
+        public void Add(User newUser)
+        {
+            var users = GetALl();
+            users.Add(newUser);
+
+            var jsonData = JsonConvert.SerializeObject(users);
+            FileProvider.Replace(Path, jsonData);
+        }
+    }
+}
